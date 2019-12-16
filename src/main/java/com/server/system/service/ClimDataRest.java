d37d3310f1381ea051ab1ec7eedc64602059aa16
yamid <yamidcuetomazo@gmail.com>
2019-12-16 15:22:27 -0500

    update aspect

---
+++ src/main/java/com/server/system/service/ClimDataRest.java
@@ -0,0 +1,21 @@
+package com.server.system.service;
+
+import com.server.system.domain.ClimData;
+import com.server.system.persistence.ClimDataRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/climDataService")
+public class ClimDataRest {
+
+    @Autowired
+    private ClimDataRepo repo;
+
+    @GetMapping("/{year}")
+    public Iterable<ClimData> getByYear(@PathVariable("year") String year, @RequestHeader String tenantId) {
+        return repo.getByYear(year);
+    }
+
+
+}
